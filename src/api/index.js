4f29f88b2c17278ffbe2b3adbc5e433d89af3afa
Dipanshu25 <66510212+Dipanshu25@users.noreply.github.com>
2022-07-31 23:13:47 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-const url = "http://localhost:5007/posts";
+const url = "http://localhost:5007/post";
 
 export const createPost = (newPost) => axios.post(url, newPost);
